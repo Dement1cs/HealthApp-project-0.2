d672f00f80be95a29c2837aa4518feb304e3a16a
Metall1st <93581822+Dement1cs@users.noreply.github.com>
2023-12-06 00:02:33 +0000

    Add files via upload

---
+++ ProjectOOP/src/main/java/com/mycompany/emptyproject/HealthApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.emptyproject;
+
+/**
+ *
+ * @author zador
+ */
+public class HealthApp {
+    public static void main(String[] args) {
+        ProfileGUI profileGUI = new ProfileGUI();
+        profileGUI.setVisible(true);
+
+        FAQGUI faqGUI = new FAQGUI();
+        faqGUI.setVisible(true);
+
+        ForumGUI forumGUI = new ForumGUI();
+        forumGUI.setVisible(true);
+    }
+}
+
